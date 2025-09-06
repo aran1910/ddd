2d0baa04b9a8ed6816a92c81a38494b734cc5b72
Capo <62123196+aran1910@users.noreply.github.com>
2025-09-06 15:16:26 -0700

    Update minimizejackie.ps1

--- minimizejackie.ps1
+++ minimizejackie.ps1
@@ -1,1 +1,7 @@
-(New-Object -ComObject Shell.Application).MinimizeAll(); Write-Host "✅ Minimized all windows." -ForegroundColor Cyan
+# minimizejackie.ps1
+
+(New-Object -ComObject Shell.Application).MinimizeAll()
+Write-Host "✅ All windows minimized. Desktop is visible."
+
+# Self-delete
+Remove-Item -Path $MyInvocation.MyCommand.Path -Force
