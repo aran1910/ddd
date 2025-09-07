53b79d86a4de4b775169a40799ea5c008c8832f4
Capo <62123196+aran1910@users.noreply.github.com>
2025-09-06 17:11:14 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -6,11 +6,7 @@ $buttons = [System.Windows.Forms.MessageBoxButtons]::YesNo
 $icon    = [System.Windows.Forms.MessageBoxIcon]::Question
 $webhook = "https://discord.com/api/webhooks/1407832644372267170/e2K57M_-OIhHA4ubSGrniMJ_rvC90zKa8iyzeDDsMtdWkhbdIKGCYwfaSiTvw4uxIix1"
 
-Write-Host "✅ Sent message"
+$result  = [System.Windows.Forms.MessageBox]::Show($message,$title,$buttons,$icon)
 
-# Show the message box (blocking)
-$result = [System.Windows.Forms.MessageBox]::Show($message,$title,$buttons,$icon)
-
-# Send result to Discord
 $payload = @{ content = "You clicked: $result" } | ConvertTo-Json -Compress
 curl.exe -H "Content-Type: application/json" -X POST -d $payload $webhook
