ae20152d9cd9ae786e3872c3ec8a1fd46022f3c9
Capo <62123196+aran1910@users.noreply.github.com>
2025-09-06 17:06:02 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,22 +1,20 @@
 Add-Type -AssemblyName System.Windows.Forms
 
-# Customize
 $title   = "Confirm"
 $message = "Do you like this demo?"
 $buttons = [System.Windows.Forms.MessageBoxButtons]::YesNo
 $icon    = [System.Windows.Forms.MessageBoxIcon]::Question
 $webhook = "https://discord.com/api/webhooks/1407832644372267170/e2K57M_-OIhHA4ubSGrniMJ_rvC90zKa8iyzeDDsMtdWkhbdIKGCYwfaSiTvw4uxIix1"
 
-# Immediately print so it doesn't stall
 Write-Host "✅ Sent message"
 
-# Run message box in separate thread so it's non-blocking
-$action = [System.Threading.ThreadStart]{
-    $result  = [System.Windows.Forms.MessageBox]::Show($message,$title,$buttons,$icon)
+$action = {
+    Add-Type -AssemblyName System.Windows.Forms
+    $result  = [System.Windows.Forms.MessageBox]::Show($using:message,$using:title,$using:buttons,$using:icon)
     $payload = @{ content = "You clicked: $result" } | ConvertTo-Json -Compress
     curl.exe -H "Content-Type: application/json" -X POST -d $payload $using:webhook
 }
 
-$thread = [System.Threading.Thread]::new($action)
-$thread.SetApartmentState([System.Threading.ApartmentState]::STA)
+$thread = New-Object System.Threading.Thread ($action)
+$thread.SetApartmentState('STA')
 $thread.Start()
